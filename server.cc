bec168d67b8b2856dc1a8872b8df3ea5c0a90039
p02lonad <p02lonad@uco.es>
2023-10-24 20:21:24 +0200

    Git bugged lol

--- server.cc
+++ server.cc
@@ -256,21 +256,13 @@ bool Server::start(){
                                 }
                                 if(checkLogin(login))
                                 {
-<<<<<<< HEAD
-                                    sprintf(buffer, "-Err. Usuario ya registrado\n");
-=======
                                     sprintf(buffer, "Usuario ya registrado\n");
->>>>>>> refs/remotes/origin/main
                                 }
                                 else
                                 {
                                     addLogin(login, password);
                                     pushbackValid(login);
-<<<<<<< HEAD
-                                    sprintf(buffer, "+Ok. Usuario validado\n");
-=======
                                     sprintf(buffer, "Usuario validado\n");
->>>>>>> refs/remotes/origin/main
                                     flag_singup = true;
                                 }
                                 
